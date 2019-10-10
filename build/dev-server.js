995ea181354209c193069c794e365b742f6dac08
Shaohua Wen <shaohua.wen@gmail.com>
2019-10-10 13:26:11 +0800

    update package version and enable hot recompile and reload page in dev mode

--- build/dev-server.js
+++ build/dev-server.js
@@ -34,14 +34,12 @@ const hotMiddleware = require('webpack-hot-middleware')(compiler, {
   heartbeat: 2000
 })
 // force page reload when html-webpack-plugin template changes
-// currently disabled until this is resolved:
-// https://github.com/jantimon/html-webpack-plugin/issues/680
-// compiler.plugin('compilation', function (compilation) {
-//   compilation.plugin('html-webpack-plugin-after-emit', function (data, cb) {
-//     hotMiddleware.publish({ action: 'reload' })
-//     cb()
-//   })
-// })
+compiler.plugin('compilation', function (compilation) {
+  compilation.plugin('html-webpack-plugin-after-emit', function (data, cb) {
+    hotMiddleware.publish({ action: 'reload' })
+    cb()
+  })
+})
 
 // enable hot-reload and state-preserving
 // compilation error display
